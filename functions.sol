ea700dc7d4ec38d26a38870654d157a92b98a465
Lance Malig <thomasclancy39@gmail.com>
2025-11-18 14:17:13 +0800

    Create functions.sol

---
+++ functions.sol
@@ -0,0 +1,14 @@
+// Made by Lance Malig
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+    //declares the start of a Solidity contract named "FunctionIntro"
+contract FunctionIntro {
+    //Functions can return multiple values.
+    function add(uint x, uint y) external pure returns (uint) {
+        return (x + y);
+    }
+
+    function sub (uint x, uint y) external pure returns (uint) {
+        return (x - y);
+    }
+}
